314fbbc3e245abea841ff3be1e5bd1dc8da089e6
Paidi-Pavan-Kumar <paidipavanchowdary@gmail.com>
2024-12-25 08:57:41 +0530

    Permutation Sequence

--- Q_060.java
+++ Q_060.java
@@ -45,8 +45,53 @@ class BruteTLE {
     // Auxiliary Space:
     // Recursion stack: O(n), due to the depth of the recursion tree.
     // arr is reused and does not contribute additional space.
-    
+
     // Result Storage:
     // O(n! × n), storing all permutations in the list.
     // Total Space Complexity: O(n! × n) (dominates the recursion stack).
+}
+
+class Optimal {
+
+    public String getPermutation(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        int fact = 1;
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+            list.add(i);
+        }
+        list.add(n);
+        StringBuilder res = new StringBuilder();
+        k = k - 1; // 0 based indexing
+        while (true) {
+            res.append(list.get(k / fact));
+            list.remove(k / fact);
+            if (list.size() == 0)
+                break;
+            k = k % fact;
+            fact = fact / list.size();
+        }
+
+        return res.toString();
+    }
+
+    // Complexity Analysis:
+    // Time Complexity:
+    // O(n²)
+
+    // Building the initial list: Takes O(n).
+    // Iterative process:
+    // For each of the n iterations, removing an element from the list takes O(n) in
+    // the worst case.
+    // Total complexity for this operation is O(n²).
+    // Space Complexity:
+    // Auxiliary Space:
+
+    // The list stores up to n elements: O(n).
+    // The StringBuilder holds the result: O(n).
+    // Recursion Stack:
+
+    // No recursion is used.
+    // Total Space Complexity: O(n)
+
 }
